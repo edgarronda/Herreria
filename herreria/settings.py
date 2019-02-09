1fdd2df19da3062ecc6a69299808e35f45f77c16
Edgar Ronda <edgar.g.ronda@gmail.com>
2019-02-08 17:03:19 -0800

    Adding Listings and Workers apps.

--- herreria/settings.py
+++ herreria/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'workers.apps.WorkersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
