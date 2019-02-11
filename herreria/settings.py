6924a3a88041ca84caeeac009405059f72bce68a
Edgar Ronda <edgar.g.ronda@gmail.com>
2019-02-11 15:16:54 -0800

    Listings and Workers migrations.

--- herreria/settings.py
+++ herreria/settings.py
@@ -77,10 +77,23 @@ WSGI_APPLICATION = 'herreria.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#Prod
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+#Local Dev
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'herreria',
+         'USER': '',
+         'PASSWORD': '',
+         'HOST': 'localhost', # '127.0.0.1' probably works also
+         'PORT': '5432',
     }
 }
 
