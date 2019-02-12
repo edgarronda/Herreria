584632f06f321ccb82ab41fea37c0c1ca3c577d9
Edgar Ronda <edgar.g.ronda@gmail.com>
2019-02-12 11:22:31 -0800

    Custom pagination. Now works.

--- listings/views.py
+++ listings/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from .models import Listing
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 # Create your views here.
 def index(request):
 	#Get all listings from DB.
-	listings = Listing.objects.all()
+	listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+	paginator = Paginator(listings, 6)
+	page = request.GET.get('page')
+	paged_listings = paginator.get_page(page)
+
 	#Generate list to the view.
 	context = {
-		'listings' : listings
+		'listings' : paged_listings
 	}
 
 	return render(request, 'listings/listings.html', context)
