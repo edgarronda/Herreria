0ad35795f0631b43cd26c9927dfff6a1c33d0ba0
Edgar Ronda <edgar.g.ronda@gmail.com>
2019-02-12 13:58:16 -0800

    Changed about content to dinamic content.

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from listings.models import Listing
+from workers.models import Worker
 
 # Create your views here.
 def index(request):
-	return render(request, 'pages/index.html')
+	listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+	context = {
+		'listings': listings
+	}	
+
+	return render(request, 'pages/index.html', context)
 
 
 def about(request):
-	return render(request, 'pages/about.html')
+	#Get all workers.
+	workers = Worker.objects.order_by('-hire_date')
+
+	#Get MVP Workers
+	mvp_workers = Worker.objects.all().filter(is_mvp=True)
+
+	context = {
+		'workers': workers,
+		'mvp_workers': mvp_workers
+	}
+
+	return render(request, 'pages/about.html', context)
 	
