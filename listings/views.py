2dfcf1d99546e528a79257468a87e3e840d23e7a
Edgar Ronda <edgar.g.ronda@gmail.com>
2019-02-12 16:33:36 -0800

    Implement search inside index and dedicated page.

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, listingtype_choices, state_choices
 
 
 # Create your views here.
 def index(request):
-	#Get all listings from DB.
 	listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
 	paginator = Paginator(listings, 6)
@@ -20,8 +20,9 @@ def index(request):
 	return render(request, 'listings/listings.html', context)
 
 
+#Listing all publications.
 def listing(request, listing_id):
-	listing = get_object_or_404(Listing, pk=listing_id)
+	listing = get_object_or_404(Listing, pk=listing_id)	
 
 	context = {
 		'listing': listing,
@@ -30,5 +31,47 @@ def listing(request, listing_id):
 	return render(request, 'listings/listing.html', context)
 
 
-def search(request):
-	return render(request, 'listings/search.html')
+#Search inside listings by filters.
+def search(request):	
+	queryset_list = Listing.objects.order_by('-list_date')
+
+	#Keywords
+	if 'keywords' in request.GET:
+		keywords = request.GET['keywords']
+		if keywords:
+			queryset_list = queryset_list.filter(description__icontains=keywords)
+
+	#City
+	if 'city' in request.GET:
+		city = request.GET['city']
+		if city:
+			queryset_list = queryset_list.filter(city__iexact=city)
+
+	#State (Will be changed to colonias instead states)
+	# if 'state' in request.GET:
+	# 	state = request.GET['state']
+	# 	if state:
+	# 		queryset_list = queryset_list.filter(state__iexact=state)
+
+	#Listing Type
+	if 'listingtype' in request.GET:
+		listingtype = request.GET['listingtype']
+		if listingtype:
+			queryset_list = queryset_list.filter(listingtype__iexact=listingtype)
+
+	#Price
+	if 'price' in request.GET:
+		price = request.GET['price']
+		if price:
+			queryset_list = queryset_list.filter(price__lte=price)
+
+
+	context = {
+		'state_choices': state_choices,
+		'listingtype_choices': listingtype_choices,
+		'price_choices': price_choices,
+		'listings': queryset_list,
+		'values': request.GET
+	}	
+
+	return render(request, 'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
 from workers.models import Worker
+from listings.choices import price_choices, listingtype_choices, state_choices
 
 # Create your views here.
 def index(request):
 	listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
 	context = {
-		'listings': listings
+		'listings': listings,
+		'state_choices': state_choices,
+		'listingtype_choices': listingtype_choices,
+		'price_choices': price_choices
 	}	
 
 	return render(request, 'pages/index.html', context)
