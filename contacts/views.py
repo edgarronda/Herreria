014ffc5db03ba1be0f67e9fd575354d72b9cd4ce
Edgar Ronda <edgar.g.ronda@gmail.com>
2019-02-22 11:54:06 -0800

    Inquiry validation and send email.

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 
@@ -15,10 +16,31 @@ def contact(request):
 		user_id = request.POST['user_id']
 		worker_email = request.POST['worker_email']
 
+
+		#Worker already have inquery.
+		if request.user.is_authenticated:
+			user_id = request.user.id
+			has_contacted = Contact.objects.all().filter(listing_id =listing_id, user_id = user_id)
+			if has_contacted:
+				messages.error(request, 'Ya solicitaste una cotizacion para este producto.')
+				return redirect('/listings/'+listing_id)
+
+
 		contact = Contact(listing = listing, listing_id = listing_id, name = name, email = email, 
 		phone = phone, message = message, user_id = user_id)
 
 		contact.save()
+
+		#Send Email
+		send_mail(
+			'Cotizacion de Producto',
+			'Se requiere una cotizacion para el producto: ' + listing + '. Ingresa al panel para mas informacion',
+			'test_email@gmail.com',
+			[worker_email, 'trabajador@gmail.com'],
+			fail_silently = False
+			)
+
+
 
 		messages.success(request, 'Tu informacion a sido enviada, pronto se te contactara.')
 
--- herreria/settings.py
+++ herreria/settings.py
@@ -154,3 +154,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+
+#Email Config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'test_email@gmail.com'
+EMAIL_HOST_PASSWORD = '1234'
+EMAIL_USE_TLS = True
